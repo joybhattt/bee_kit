0e2532b6f06bac0c1cf73eaf956f3efcfcc0421f
_ Joy <88931177+joybhattt@users.noreply.github.com>
2026-01-23 19:45:22 +0530

    house keeping

--- src/essentials/utility/root.zig
+++ src/essentials/utility/root.zig
@@ -1,4 +1,3 @@
 pub const LoopThread = @import("looped_thread.zig").LoopThread;
 pub const HelpFuncs = @import("helper_functions.zig");
-pub const FpsTimer = @import("fps_timer.zig");
 pub const Err = @import("error.zig");

72a701d8a7c9764f0ff34011a5b05ba4bb185f06
joybhattt <joy321bhatt@gmail.com>
2026-01-23 16:28:28 +0000

    house keeping

--- build.zig
+++ build.zig
@@ -4,14 +4,14 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const root_module = b.addModule("bee_engine",.{
+    const root_module = b.addModule("bee_kit",.{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
 
     const lib = b.addLibrary(.{
-        .name = "bee_engine",
+        .name = "bee_kit",
         .root_module = root_module,
         .linkage = .static,
     });

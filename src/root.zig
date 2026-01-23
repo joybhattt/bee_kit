b448b035af33352b2b31e873f07dbe8d6b6944dd
joybhattt <joy321bhatt@gmail.com>
2026-01-23 16:31:21 +0000

    house keeping

--- src/root.zig
+++ src/root.zig
@@ -1,14 +1,5 @@
 // -------------------------------------------------------------------------------------------------
 // Engine Modules
 // -------------------------------------------------------------------------------------------------
-pub const Network = @import("essentials/network/root.zig");
-
-pub const Physics = @import("essentials/physics/root.zig");
-
-pub const Utility = @import("essentials/utility/root.zig");
-
-pub const Graphics = @import("enviornment/graphics/root.zig");
-
-pub const Sound = @import("enviornment/sound/root.zig");
-
-pub const Window = @import("enviornment/sound/root.zig");
+pub const Physics = @import("physics/root.zig");
+pub const Utility = @import("utility/root.zig");
